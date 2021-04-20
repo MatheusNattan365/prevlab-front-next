7340b2e69f2dcb1ce7f7d0eb37d4b7fe95c35227
matheus <nanamprogrammer@gmail.com>
2021-04-19 23:54:17 -0300

    [feat]: dashboard structure

--- components/admin/dashboard/MainContent.js
+++ components/admin/dashboard/MainContent.js
@@ -30,7 +30,7 @@ function Header() {
       <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8  ">
         {/* Replace with your content */}
 
-        <div className="px-4 py-4 sm:px-0 h">
+        <div className="px-4 py-4 sm:px-0">
           {currentApp(adminDashContext.app)}
         </div>
         {/* /End replace */}
--- components/admin/dashboard/contents/AdminDashboard.js
+++ components/admin/dashboard/contents/AdminDashboard.js
@@ -4,8 +4,12 @@ import TimeLine from "./components/timeLine";
 
 function AdminDashboard() {
   return (
-    <div>
-      <TimeLine />
+    <div className="flex">
+      <div className="flex-grow">
+        <div className="container" style={{ height: 700, overflow: "auto" }}>
+          <TimeLine />
+        </div>
+      </div>
     </div>
   );
 }
