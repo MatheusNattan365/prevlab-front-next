1ffae20be24afaed55dc0160f54e71dd3c6d0fb0
matheusnattan365 <nanamprogrammer@gmail.com>
2022-11-27 18:37:47 -0300

    forget proccess.env

--- services/axiosConfig.js
+++ services/axiosConfig.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // axios.defaults.baseURL = "http://localhost:3333/api";
 axios.defaults.baseURL =
     process.env.NEXT_PUBLIC_PREVLAB_HEROKU_URL ||
-    NEXT_PUBLIC_PREVLAB_ONRENDER_URL ||
+    process.env.NEXT_PUBLIC_PREVLAB_ONRENDER_URL ||
     "http://localhost:3333/api";
 
 export { axios };
